a65dde6584e9c5e42d371821c58c8c44f70e1a96
Daniel Tan <melliniumorder@gmail.com>
2015-04-20 21:46:46 +0800

    add check by type or language

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,83 @@ var router = express.Router();
 /* New parsing method */
 router.get('/',function(req,res){
   seriesTitleFilterByDownload(req,res);
+  seriesLanguageFilterByDownload(req,res);
   //console.log(res);
 })
 
 function seriesLanguageFilterByDownload(req,res){
-  //To do:
+  var postdata=req.query;
+  if(postdata.language && postdata.type){
+    var titletype=capitalizeFirstLetter(postdata.type.toLowerCase());
+    var language =capitalizeFirstLetter(postdata.language.toLowerCase());
+    var category =titletype+"_("+language+")";
+    download("http://www.baka-tsuki.org/project/api.php?action=query&cmlimit=400&format=json&list=categorymembers&cmtitle=Category:"+category, function(resd){
+      var data={};
+      var jsondata=JSON.parse(resd);
+      var serieslist=jsondata.query.categorymembers;
+      data.type=titletype;
+      data.language=language;
+      data.titles=[];
+      for(var key in serieslist){
+        var title=serieslist[key].title;
+        data.titles[key]={};
+        data.titles[key].page=title.replace(/ /g,"_");
+        data.titles[key].title=title;
+      }
+      res.send(data);
+    })    
+  }else if(postdata.language && !postdata.type){
+    //Only provide a list of title types for the language
+    var language =capitalizeFirstLetter(postdata.language.toLowerCase());
+    download("http://www.baka-tsuki.org/project/api.php?action=query&cmlimit=400&format=json&list=categorymembers&cmtitle=Category:"+language, function(resd){
+      var data={};
+      var jsondata=JSON.parse(resd);
+      var serieslist=jsondata.query.categorymembers;
+      data.language=language;
+      data.types=[];
+      for(var key in serieslist){
+        if(serieslist[key].title.match(/Category/g)){
+          var typeoftitle=serieslist[key].title.replace(/Category:/g,"").split(/ /g);
+          typeoftitle.pop();
+          data.types.push(typeoftitle.join("_"));
+        }
+      }
+      res.send(data);
+    })
+  }else if(postdata.type && !postdata.type.match(/Original_?novel/i) && !postdata.language){
+    //Provide languages available for that type.
+    var titletype =capitalizeFirstLetter(postdata.type.toLowerCase());
+    download("http://www.baka-tsuki.org/project/api.php?action=query&cmlimit=400&format=json&list=categorymembers&cmtitle=Category:"+titletype, function(resd){
+      var data={};
+      var jsondata=JSON.parse(resd);
+      var serieslist=jsondata.query.categorymembers;      
+      data.types=titletype;
+      data.language=[];
+      for(var key in serieslist){
+        if(serieslist[key].title.match(/Category/g)){
+          var language=serieslist[key].title.match(/\((.+)\)/g,"")[0].replace(/[\(\)]/g, "");
+          data.language.push(language);
+        }
+      }
+      res.send(data);
+    })
+  }else if(postdata.type.match(/Original_?novel/i)){
+    //Directly provide all Original Novels available as they are not divided by language.
+    download("http://www.baka-tsuki.org/project/api.php?action=query&cmlimit=400&format=json&list=categorymembers&cmtitle=Category:Original_novel", function(resd){
+      var data={};
+      var jsondata=JSON.parse(resd);
+      var serieslist=jsondata.query.categorymembers;
+      data.type="Original novel";
+      data.titles=[];
+      for(var key in serieslist){
+        var title=serieslist[key].title;
+        data.titles[key]={};
+        data.titles[key].page=title.replace(/ /g,"_");
+        data.titles[key].title=title;
+      }
+      res.send(data);
+    })
+  }
 }
 
 function seriesTitleFilterByDownload(req,res){
@@ -158,10 +230,11 @@ function seriesTitleFilterByDownload(req,res){
         res.send(data); 
       }      
     });
-  }else{
-    //Future cache here.
-    //res.send("Please show which volume you want.");
   }
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function stripNumbering(line){
