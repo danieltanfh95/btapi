eba4bb611eb6f50564ba8181d287f195199a1551
Daniel Tan <melliniumorder@gmail.com>
2015-04-21 10:34:19 +0800

    try id

--- routes/api.js
+++ routes/api.js
@@ -139,7 +139,7 @@ function seriesTitleFilterByDownload(req,res){
 
         //Completed Preloading of Data
         //Get data about available volumes from the toc
-        console.log($(".toc ul li").text());
+        console.log($(".toc ul li, #toc ul li").text());
         $(".toc ul li").each(function(){          
           //Notes that each page format has its own quirks and the program attempts to match all of them
           console.log($(this).text());
